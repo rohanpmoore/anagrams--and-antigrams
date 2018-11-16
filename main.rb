036e6cc98fee807944938b7022a7e1b734e904cf
Rohan Moore <rohanpmoore@gmail.com>
2018-11-16 13:20:10 -0800

    add terminal-based user interface

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
 require './lib/anagram'
+
+print "\nWelcome to Anagrams!\n"
+print "This program takes two words or sentenes and tells you whether they are anagrams!\n"
+print "Let's begin!\n\n"
+continue = "y"
+until continue == "n"
+  print "Enter the first word or sentence: "
+  word_one = gets.chomp
+  print "Enter the second word or sentence: "
+  word_two = gets.chomp
+  result = anagram(word_one, word_two)
+  print "#{result}\n"
+  continue = "x"
+  while (continue != "y") & (continue != "n")
+    print "Would you like to continue (y/n): "
+    continue = (gets.chomp).downcase
+  end
+end
+print "Have a wonderful day!\n"
