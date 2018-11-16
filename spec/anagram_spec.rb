b9bf2eeb626963dffe0f98037a3cec62d53f0f34
Rohan Moore <rohanpmoore@gmail.com>
2018-11-16 11:13:24 -0800

    add spec 3: fails

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -8,4 +8,7 @@ describe('anagram') do
   it('will take two words and check if they are anagrams, ignoring case') do
     expect(anagram("resTFUL", "FLUster")).to(eq("resTFUL and FLUster are anagrams."))
   end
+  it('will tell you if you did not input a real word') do
+    expect(anagram("ruby", "brrt")).to(eq("one or more inputs are not words."))
+  end
 end
