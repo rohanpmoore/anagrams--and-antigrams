6a32f25b0fe2e1764b048a88b94dbc1c8b9c5bff
Rohan Moore <rohanpmoore@gmail.com>
2018-11-16 13:54:44 -0800

    rough draft sinatra

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+also_reload('lib/**/*.rb')
+require './lib/anagram'
 
 get('/') do
   @description = "This application will determine whether two words/phrases are anagrams."
   erb(:main)
+end
+
+get('/output') do
+  word_one = params.fetch("word-one")
+  word_two = params.fetch("word-two")
+  @result = anagram(word_one, word_two)
+  erb(:output)
 end
