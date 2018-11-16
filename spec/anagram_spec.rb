b98f40590f3db169ad6709d3d26d110d1cdfa915
Rohan Moore <rohanpmoore@gmail.com>
2018-11-16 09:40:36 -0800

    add spec one, fail

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,1 +1,8 @@
 require 'rspec'
+require 'anagram'
+
+describe('anagram') do
+  it('will take two words and check if they are anagrams') do
+    expect(anagram("ruby", "bury")).to(eq(true))
+  end
+end
